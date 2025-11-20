3dbe5e2d004df721398733b4075801f8fa31dc3e
david111691 <davidchaikovsky100@gmail.com>
2025-11-20 16:13:10 +0200

    added store with redux

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import PostFeed from "@/components/PostFeed";
 import SideBar from "@/components/SideBar";
+import SignUpPromt from "@/components/SignUpPromt";
 import Widgets from "@/components/Widgets";
 
 export default function Home() {
   return (
-    <div className="text-[#0F1419] min-h-screen border-2 border-black max-w-[1400px] mx-auto flex">
-      <SideBar />
-      <PostFeed />
-      <Widgets />
-    </div>
+    <>
+      <div className="text-[#0F1419] min-h-screen max-w-[1400px] mx-auto flex justify-center">
+        <SideBar />
+        <PostFeed />
+        <Widgets />
+      </div>
+      <SignUpPromt />
+    </>
   );
 }
